4c33edcec6bc5add83d8f6f1755c87b895b025b5
Ali Ariff <ali@bukalapak.com>
2016-01-26 12:57:19 +0700

    update

--- pclp/wrapper.c
+++ pclp/wrapper.c
@@ -1,20 +1,19 @@
 #include <stdlib.h>
-  #include <sys/types.h>
-  #include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-  int
-  main (int argc, char *argv[])
-  {
-     setuid (0);
+int main (int argc, char *argv[])
+{
+  setuid (0);
 
-     /* WARNING: Only use an absolute path to the script to execute,
-      *          a malicious user might fool the binary and execute
-      *          arbitary commands if not.
-      * */
+  /* WARNING: Only use an absolute path to the script to execute,
+  *          a malicious user might fool the binary and execute
+  *          arbitary commands if not.
+  * */
 
-     	system ("kill -9 $(ps aux | grep 'bots.py' | grep -v grep | awk '{print $2}')");
-	system ("kill -9 $(ps aux | grep 'rejudge.py' | grep -v grep | awk '{print $2}')");
-	system ("kill -9 $(ps aux | grep 'main.py' | grep -v grep | awk '{print $2}')");
+  system ("kill -9 $(ps aux | grep 'bots.py' | grep -v grep | awk '{print $2}')");
+  system ("kill -9 $(ps aux | grep 'rejudge.py' | grep -v grep | awk '{print $2}')");
+  system ("kill -9 $(ps aux | grep 'main.py' | grep -v grep | awk '{print $2}')");
 
-     return 0;
-   }
+  return 0;
+}
