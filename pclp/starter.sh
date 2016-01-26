30f720c26625656358c6a91a8308ddac331898f5
Ali Ariff <ali@bukalapak.com>
2016-01-26 16:24:22 +0700

    update

--- pclp/starter.sh
+++ pclp/starter.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+cd compilerDaemonMk4b
+
 if ps aux | grep "[b]ots.py" > /dev/null
 then
     :
 else
-    python /root/pclp/compilerDaemonMk4b/bots.py &
+    python bots.py &
 fi
 
 if ps aux | grep "[m]ain.py" > /dev/null
 then
     :
 else
-    python /root/pclp/compilerDaemonMk4b/main.py &
+    python main.py &
 fi
 
 if ps aux | grep "[r]ejudge.py" > /dev/null
 then
     :
 else
-    python /root/pclp/compilerDaemonMk4b/rejudge.py &
+    python rejudge.py &
 fi
