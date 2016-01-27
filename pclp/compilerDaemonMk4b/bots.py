79fba62be4eb8b44fb8f2e58142ad22d6cd5fc2d
Ali Ariff <ali@bukalapak.com>
2016-01-27 11:44:00 +0700

    fix bot

--- pclp/compilerDaemonMk4b/bots.py
+++ pclp/compilerDaemonMk4b/bots.py
@@ -6,14 +6,21 @@ import threading
 import os
 import paths
 import database
+import re
 
 db = _mysql.connection(host=database.host, user=database.user, passwd=database.passwd, db=database.dbname)
 
 while True:
+    pat = re.compile('index.html')
     try:
         list1 = os.listdir(paths.statusPath)
         list1.sort()
         for i in list1:
+            if i[0] == '.':
+                continue
+            if pat.search(i) != None:
+                continue
+
             data = i.split('.')
             db.query("UPDATE pc_submit SET SCORE = '" + data[2]
                      + "', STATUS_ID = '" + data[3]
