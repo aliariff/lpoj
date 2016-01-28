0e29cbe77fb2bb4cdcc8cb863f1d61cbf99ec7fe
Ali Ariff <ali@bukalapak.com>
2016-01-28 13:19:06 +0700

    refactor compiler

--- pclp/compilerDaemonMk4b/Compiler.py
+++ pclp/compilerDaemonMk4b/Compiler.py
@@ -40,6 +40,18 @@ class Compiler:
 
         self.tmpPath = paths.tmpPath
         self.compiled = 0
+        self.total = 0
+        self.submit_log = ''
+        self.status_log = '.7.'
+
+        limiter = open(paths.limiterPath + self.soal, 'r')
+        limit = limiter.readline()
+        self.limit = float(limit)
+        limiter.close()
+        memoryL = open(paths.memoryPath + self.soal, 'r')
+        self.memo = memoryL.readline()
+        memoryL.close()
+
         self.db = MySQLdb.connect(host=database.host,
                                   user=database.user,
                                   passwd=database.passwd,
@@ -92,43 +104,14 @@ class Compiler:
     def test(self):
         returnStat = 0
         if self.compiled != 1:
-            logs = open(Compiler.log, 'a')
-            status = open(paths.rootCompilerPath + 'status/'
-                          + self.waktu + '.' + self.hashcode + '.0.2.2'
-                          , 'w')
-            status.close()
-            logs.write('kompilasi ' + self.hashcode + ' '
-                       + self.filename + '.' + self.filetype + ' '
-                       + self.user + ' ' + self.soal
-                       + ' GAGAL, COMPILE ERROR\n')
-            logs.close()
+            self.ce()
             return
         else:
-            limiter = open(paths.limiterPath + self.soal, 'r')
-            limit = limiter.readline()
-            limit = float(limit)
-            limiter.close()
-            memoryL = open(paths.memoryPath + self.soal, 'r')
-            memo = memoryL.readline()
-            memoryL.close()
-
             counter = len([name for name in
                           os.listdir(paths.rootCompilerPath
                           + 'inputcase/') if self.soal in name])
-            total = 0
-            submit_log = ''
-            status_log = '.7.'
-
-            # langkah menjalankan program
 
             if self.filetype == 'java':
-                limiter = open(paths.limiterPath + self.soal, 'r')
-                memoryL = open(paths.memoryPath + self.soal, 'r')
-                limit = limiter.readline()
-                limiter.close()
-                memory = memoryL.readline()
-                memoryL.close()
-                execPath = os.getcwd()
                 os.chdir(self.outputPath)
 
                 returnStat = os.system('java -Duser.language=EN '
@@ -146,133 +129,162 @@ class Compiler:
                                   + '.0', 'w')
                     status.close()
                     return
+            elif self.filetype == 'py':
+                for i in range(1, counter + 1):
+                    print 'i = ' + str(i)
+                    returnStat = os.system('python ' + self.filename
+                            + '<' + self.testPath + self.soal + '_'
+                            + str(i) + '>' + self.tmpPath + '/tempOut &'
+                            )
+                    time.sleep(float(limit))
+                    os.system('ps -C python -o pid= > pid')
+                    if os.path.getsize('./pid') != 0:
+                        pid = open('./pid')
+                        os.system('kill ' + pid.readline())
+                        pid.close()
+                        self.tle(i)
+                        continue
+            elif self.filetype == 'rb':
+                pass
             elif self.filetype == 'cpp' or self.filetype == 'c':
-                limit = limit * 1000
+                self.limit = self.limit * 1000
                 for i in range(1, counter + 1):
                     print 'i = ' + str(i)
                     s = Sandbox(self.outputPath + 'out',
                                 stdin=open(self.testPath + self.soal
                                 + '_' + str(i), 'r'),
                                 stdout=open(self.tmpPath + '/tempOut',
-                                'w'), quota=dict(cpu=int(limit),
-                                memory=int(memo), wallclock=int(limit)))
+                                'w'), quota=dict(cpu=int(self.limit),
+                                memory=int(self.memo),
+                                wallclock=int(self.limit)))
                     s.run()
                     y = s.probe()
                     if s.result == 3:
-                        submit_log += '3 '
-                        status_log = '.5.'
-                        logs = open(Compiler.log, 'a')
-                        logs.write('running ' + self.hashcode + ' '
-                                   + self.filename + '.'
-                                   + self.filetype + ' ' + self.user
-                                   + ' ' + self.soal + ' testcase'
-                                   + str(i)
-                                   + ' GAGAL, Memory Limit Exceeded\n')
-                        logs.close()
+                        self.mle(i)
                         continue
                     if s.result == 5:
-                        submit_log += '5 '
-                        status_log = '.4.'
-                        logs = open(Compiler.log, 'a')
-                        logs.write('running ' + self.hashcode + ' '
-                                   + self.filename + '.'
-                                   + self.filetype + ' ' + self.user
-                                   + ' ' + self.soal + ' testcase'
-                                   + str(i)
-                                   + ' GAGAL, Time Limit Exceeded\n')
-                        logs.close()
+                        self.tle(i)
                         continue
                     if s.result == 6:
-                        submit_log += '6 '
-                        status_log = '.3.'
-                        logs = open(Compiler.log, 'a')
-                        logs.write('running ' + self.hashcode + ' '
-                                   + self.filename + '.'
-                                   + self.filetype + ' ' + self.user
-                                   + ' ' + self.soal + ' testcase'
-                                   + str(i) + ' GAGAL, Runtime Error\n')
-                        logs.close()
+                        self.rte(i)
                         continue
-
-                    print 'diff ' + self.tmpPath + '/tempOut ' \
-                        + self.inputPath + self.soal + '_' + str(i) \
-                        + ' > ' + self.tmpPath + 'tmpLog'
-
-                    returnStat = 0
-                    p = 0
-                    toll = open(paths.tollerancepath + self.soal, 'r')
-                    try:
-                        p = float(toll.read())
-                    except ValueError:
-                        print 'exception'
-                    toll.close()
-                    if p == 0:
-                        returnStat = os.system('diff ' + self.tmpPath
-                                + '/tempOut ' + self.inputPath
-                                + self.soal + '_' + str(i))
-                    else:
-                        returnStat = os.system('python ./custdiff.py '
-                                + self.tmpPath + '/tempOut '
-                                + self.inputPath + self.soal + ' '
-                                + str(p))
-                    if returnStat == 0:
-                        print returnStat
-                        submit_log += '7 '
-                        logs = open(Compiler.log, 'a')
-                        logs.write('running ' + self.hashcode + ' '
-                                   + self.filename + '.'
-                                   + self.filetype + ' ' + self.user
-                                   + ' ' + self.soal + ' testcase'
-                                   + str(i) + ' SUKSES, AC\n')
-                        logs.close()
-                        print 'Sukses'
-                        cursor = self.db.cursor()
-                        sql = \
-                            "select persentase from pc_testcase where inputcase = 'testCase" \
-                            + str(self.soal) + '_' + str(i) + "'"
-                        cursor.execute(sql)
-                        results = cursor.fetchall()
-                        for row in results:
-                            print row[0]
-                            total = total + int(row[0])
-                    else:
-                        print returnStat
-                        returnStat = os.system('diff -iwB '
-                                + self.tmpPath + '/tempOut '
-                                + self.inputPath + self.soal + '_'
-                                + str(i))
-                        if returnStat == 0:
-                            submit_log += '8 '
-                            status_log = '.6.'
-                            logs = open(Compiler.log, 'a')
-                            logs.write('running ' + self.hashcode + ' '
-                                    + self.filename + '.'
-                                    + self.filetype + ' ' + self.user
-                                    + ' ' + self.soal + ' testcase'
-                                    + str(i)
-                                    + ' GAGAL, Presentation Error\n')
-                            logs.close()
-                            print 'presentation error'
-                        else:
-                            submit_log += '9 '
-                            status_log = '.1.'
-                            logs = open(Compiler.log, 'a')
-                            logs.write('running ' + self.hashcode + ' '
-                                    + self.filename + '.'
-                                    + self.filetype + ' ' + self.user
-                                    + ' ' + self.soal + ' testcase'
-                                    + str(i) + ' GAGAL, Wrong Answer\n')
-                            logs.close()
-                            print 'gagal'
+                    self.compare(i)
 
             status = open(paths.rootCompilerPath + 'status/'
                           + self.waktu + '.' + self.hashcode + '.'
-                          + str(total) + status_log + submit_log, 'w')
+                          + str(self.total) + self.status_log
+                          + self.submit_log, 'w')
             status.close()
             logs = open(Compiler.log, 'a')
             logs.write('HASIL ' + str(self.hashcode) + ' '
                        + self.filename + '.' + self.filetype + ' '
-                       + self.user + ' ' + self.soal + ' ' + str(total)
-                       + '\n')
+                       + self.user + ' ' + self.soal + ' '
+                       + str(self.total) + '\n')
             logs.close()
-            print 'total = ' + str(total)
+            print 'total = ' + str(self.total)
+
+    def compare(self, i):
+        print 'diff ' + self.tmpPath + '/tempOut ' + self.inputPath \
+            + self.soal + '_' + str(i) + ' > ' + self.tmpPath + 'tmpLog'
+
+        returnStat = 0
+        p = 0
+        toll = open(paths.tollerancepath + self.soal, 'r')
+        try:
+            p = float(toll.read())
+        except ValueError:
+            print 'exception'
+        toll.close()
+        if p == 0:
+            returnStat = os.system('diff ' + self.tmpPath + '/tempOut '
+                                   + self.inputPath + self.soal + '_'
+                                   + str(i))
+        else:
+            returnStat = os.system('python ./custdiff.py '
+                                   + self.tmpPath + '/tempOut '
+                                   + self.inputPath + self.soal + ' '
+                                   + str(p))
+        if returnStat == 0:
+            print returnStat
+            self.submit_log += '7 '
+            logs = open(Compiler.log, 'a')
+            logs.write('running ' + self.hashcode + ' ' + self.filename
+                       + '.' + self.filetype + ' ' + self.user + ' '
+                       + self.soal + ' testcase' + str(i)
+                       + ' SUKSES, AC\n')
+            logs.close()
+            print 'Sukses'
+            cursor = self.db.cursor()
+            sql = \
+                "select persentase from pc_testcase where inputcase = 'testCase" \
+                + str(self.soal) + '_' + str(i) + "'"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            for row in results:
+                print row[0]
+                self.total = self.total + int(row[0])
+        else:
+            print returnStat
+            returnStat = os.system('diff -iwB ' + self.tmpPath
+                                   + '/tempOut ' + self.inputPath
+                                   + self.soal + '_' + str(i))
+            if returnStat == 0:
+                self.submit_log += '8 '
+                self.status_log = '.6.'
+                logs = open(Compiler.log, 'a')
+                logs.write('running ' + self.hashcode + ' '
+                           + self.filename + '.' + self.filetype + ' '
+                           + self.user + ' ' + self.soal + ' testcase'
+                           + str(i) + ' GAGAL, Presentation Error\n')
+                logs.close()
+                print 'presentation error'
+            else:
+                self.submit_log += '9 '
+                self.status_log = '.1.'
+                logs = open(Compiler.log, 'a')
+                logs.write('running ' + self.hashcode + ' '
+                           + self.filename + '.' + self.filetype + ' '
+                           + self.user + ' ' + self.soal + ' testcase'
+                           + str(i) + ' GAGAL, Wrong Answer\n')
+                logs.close()
+                print 'gagal'
+
+    def mle(self, i):
+        self.submit_log += '3 '
+        self.status_log = '.5.'
+        logs = open(Compiler.log, 'a')
+        logs.write('running ' + self.hashcode + ' ' + self.filename
+                   + '.' + self.filetype + ' ' + self.user + ' '
+                   + self.soal + ' testcase' + str(i)
+                   + ' GAGAL, Memory Limit Exceeded\n')
+        logs.close()
+
+    def tle(self, i):
+        self.submit_log += '5 '
+        self.status_log = '.4.'
+        logs = open(Compiler.log, 'a')
+        logs.write('running ' + self.hashcode + ' ' + self.filename
+                   + '.' + self.filetype + ' ' + self.user + ' '
+                   + self.soal + ' testcase' + str(i)
+                   + ' GAGAL, Time Limit Exceeded\n')
+        logs.close()
+
+    def rte(self, i):
+        self.submit_log += '6 '
+        self.status_log = '.3.'
+        logs = open(Compiler.log, 'a')
+        logs.write('running ' + self.hashcode + ' ' + self.filename
+                   + '.' + self.filetype + ' ' + self.user + ' '
+                   + self.soal + ' testcase' + str(i)
+                   + ' GAGAL, Runtime Error\n')
+        logs.close()
+
+    def ce(self):
+        logs = open(Compiler.log, 'a')
+        status = open(paths.rootCompilerPath + 'status/' + self.waktu
+                      + '.' + self.hashcode + '.0.2.2', 'w')
+        status.close()
+        logs.write('kompilasi ' + self.hashcode + ' ' + self.filename
+                   + '.' + self.filetype + ' ' + self.user + ' '
+                   + self.soal + ' GAGAL, COMPILE ERROR\n')
+        logs.close()
