bcf88e7fca5ced5e134369b9d4a79547b8a9a66d
Ali Ariff <ali@bukalapak.com>
2016-01-30 11:12:03 +0700

    add syscall

--- pclp/compilerDaemonMk4b/blacklist.py
+++ pclp/compilerDaemonMk4b/blacklist.py
@@ -3,7 +3,7 @@
 import re
 
 def check(path):
-  blacklist = ['system', '`', '%60', 'eval', 'fork', 'spawn', 'exec']
+  blacklist = ['system', '`', '%60', 'eval', 'fork', 'spawn', 'exec', 'syscall']
   file = open(path, 'r')
   for line in file:
     for cmd in blacklist:
